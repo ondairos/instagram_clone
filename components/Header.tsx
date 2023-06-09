0ca46d5b000138fe1440a903a67e9773ad36ee89
Ondairos <ondairos@gmail.com>
2023-06-09 15:15:25 +0300

    added heroicons and forms from tailwind

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,16 +1,48 @@
 import React from "react";
 import Image from "next/image";
+import {
+  SearchIcon,
+  PlusCircleIcon,
+  UserGroupIcon,
+  HeartIcon,
+  PaperAirplaneIcon,
+  MenuIcon,
+} from "@heroicons/react/outline";
 
 export default function Header() {
   return (
     <header>
-      <div className="flex justify-between bg-red-500 max-w-6xl">
-        {/* left */}
-        <div className="relative h-24 w-24">
-          <Image src={"https://links.papareact.com/ocw"} alt="" fill />
+      {/* left */}
+      <div className="flex justify-between max-w-6xl cursor-pointer">
+        <div className="relative hidden lg:inline-grid w-24">
+          <Image
+            src={"https://links.papareact.com/ocw"}
+            alt=""
+            fill
+            objectFit="contain"
+          />
         </div>
 
-        {/* middle */}
+        <div className="relative w-10 lg:hidden flex-shrink-0 cursor-pointer">
+          <Image
+            src={"https://links.papareact.com/jjm"}
+            alt=""
+            fill
+            objectFit="contain"
+          />
+        </div>
+
+        {/* middle search field*/}
+        <div className="relative mt-1 p-3 rounded-md">
+          <div className="absolute inset-y-0 pl-3 flex items-center pointer-events-none">
+            <SearchIcon className="h-5 w-5 text-gray-500" />
+          </div>
+          <input
+            className="bg-gray-50 block w-full pl-10 sm:text-sm border-gray-300 focus:ring-black focus:border-black rounded-md"
+            type="text"
+            placeholder="Search"
+          />
+        </div>
 
         {/* right */}
       </div>
