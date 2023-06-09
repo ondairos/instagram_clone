972faa72866dd1794e588ff8a29b4bdd549f267c
Ondairos <ondairos@gmail.com>
2023-06-09 15:31:50 +0300

    added custom tailwind utility class

--- components/Header.tsx
+++ components/Header.tsx
@@ -8,13 +8,14 @@ import {
   PaperAirplaneIcon,
   MenuIcon,
 } from "@heroicons/react/outline";
+import { HomeIcon } from "@heroicons/react/solid";
 
 export default function Header() {
   return (
     <header>
       {/* left */}
-      <div className="flex justify-between max-w-6xl cursor-pointer">
-        <div className="relative hidden lg:inline-grid w-24">
+      <div className="flex justify-between max-w-6xl mx-5 lg:mx-auto">
+        <div className="relative hidden lg:inline-grid w-24 cursor-pointer">
           <Image
             src={"https://links.papareact.com/ocw"}
             alt=""
@@ -33,18 +34,25 @@ export default function Header() {
         </div>
 
         {/* middle search field*/}
-        <div className="relative mt-1 p-3 rounded-md">
-          <div className="absolute inset-y-0 pl-3 flex items-center pointer-events-none">
-            <SearchIcon className="h-5 w-5 text-gray-500" />
+        <div className="max-w-xs">
+          <div className="relative mt-1 p-3 rounded-md">
+            <div className="absolute inset-y-0 pl-3 flex items-center pointer-events-none">
+              <SearchIcon className="h-5 w-5 text-gray-500" />
+            </div>
+            <input
+              className="bg-gray-50 block w-full pl-10 sm:text-sm border-gray-300 focus:ring-black focus:border-black rounded-md"
+              type="text"
+              placeholder="Search"
+            />
           </div>
-          <input
-            className="bg-gray-50 block w-full pl-10 sm:text-sm border-gray-300 focus:ring-black focus:border-black rounded-md"
-            type="text"
-            placeholder="Search"
-          />
         </div>
 
         {/* right */}
+        <div className="flex items-center justify-end space-x-4">
+          <HomeIcon className="navButton" />
+          <MenuIcon className="h-6 md:hidden cursor-pointer" />
+          <PaperAirplaneIcon className="navButton" />
+        </div>
       </div>
     </header>
   );
