5eada9b9bf71c7276863180cd33c90e3a5d97194
Ondairos <ondairos@gmail.com>
2023-06-10 16:05:04 +0300

    fixed col size Stories problem with css

--- components/Feed.tsx
+++ components/Feed.tsx
@@ -4,7 +4,7 @@ import Stories from "./Stories";
 export default function Feed() {
   return (
     <main className="grid grid-cols-1 md:grid-cols-2 md:max-w-3xl xl:grid-cols-3 xl:max-w-6xl mx-auto">
-      <section>
+      <section className="col-span-2">
         {/* Stories */}
         <Stories />
         {/* Posts */}
