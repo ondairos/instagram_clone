f5d54e76cd155a736fe56541f019fcf44d9ef40a
Ondairos <ondairos@gmail.com>
2023-06-10 15:31:44 +0300

    added faker-js v6.0.0

--- components/Stories.tsx
+++ components/Stories.tsx
@@ -1,8 +1,20 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { faker } from "@faker-js/faker";
 
 export default function Stories() {
+  // populate a list of fake users
+  useEffect(() => {
+    const suggestions = [...Array(20)].map((_, index) => ({
+      ...faker.helpers.contextualCard(),
+      id: index,
+    }));
+
+    console.log(suggestions);
+  }, []);
+
   return (
     <div>
+      {/* Stories */}
       <p>Stories</p>
     </div>
   );
