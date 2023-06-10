6d9ed7facbaab2501585fce2de81c7a762fe525a
Ondairos <ondairos@gmail.com>
2023-06-10 15:11:14 +0300

    added css for main feed

--- components/Feed.tsx
+++ components/Feed.tsx
@@ -1,10 +1,12 @@
 import React from "react";
+import Stories from "./Stories";
 
 export default function Feed() {
   return (
-    <main>
+    <main className="grid grid-cols-1 md:grid-cols-2 md:max-w-3xl xl:grid-cols-3 xl:max-w-6xl mx-auto">
       <section>
         {/* Stories */}
+        <Stories />
         {/* Posts */}
       </section>
 
