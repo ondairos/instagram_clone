b2e7bee7b72a913ad16d85003f9edd2ac514e69b
Ondairos <ondairos@gmail.com>
2023-06-12 12:57:44 +0300

    added Post markup and css

--- components/Post.tsx
+++ components/Post.tsx
@@ -21,23 +21,49 @@ type Props = {
 
 export default function Post({ id, username, userImg, img, caption }: Props) {
   return (
-    <div>
-      <div>
-        <img src="" alt="" />
-        <p>{username}</p>
+    <div className="bg-white my-7 border rounded-sm">
+      {/* header */}
+      <div className="flex items-center p-5">
+        <img
+          src={userImg}
+          className="rounded-full h-12 w-12 object-contain border p-1 mr-3"
+          alt=""
+        />
+        <p className="flex-1 font-bold">{username}</p>
+        <DotsHorizontalIcon className="h-5" />
       </div>
 
-      {/* header */}
-
       {/* img */}
+      <img src={img} className="object-cover w-full" alt="" />
 
       {/* buttons */}
+      <div className="flex justify-between px-4 pt-4">
+        <div className="flex space-x-4 ">
+          <HeartIcon className="button" />
+          <ChatIcon className="button" />
+          <PaperAirplaneIcon className="button" />
+        </div>
+        <BookmarkIcon className="button" />
+      </div>
 
       {/* caption */}
+      <p className="p-5 truncate">
+        <span className="font-bold mr-1">{username} </span>
+        {caption}
+      </p>
 
       {/* comments */}
 
       {/* input box */}
+      <form className="flex items-center p-4">
+        <EmojiHappyIcon className="button" />
+        <input
+          type="text"
+          placeholder="Add a comment..."
+          className="border-none flex-1 focus:ring-0 outline-none"
+        />
+        <button className="font-semibold text-blue-400">Post</button>
+      </form>
     </div>
   );
 }
