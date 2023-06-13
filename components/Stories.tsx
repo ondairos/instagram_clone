ac4fba7c9c6e34c297d60a5d4b217a33425240e4
Ondairos <ondairos@gmail.com>
2023-06-13 15:09:21 +0300

    individual personal story button with session

--- components/Stories.tsx
+++ components/Stories.tsx
@@ -1,10 +1,13 @@
 import React, { useEffect, useState } from "react";
 import { faker } from "@faker-js/faker";
 import Story from "./Story";
+import { useSession } from "next-auth/react";
 
 export default function Stories() {
   // state
   const [suggestions, setSuggestions] = useState<SuggestionsType[]>([]);
+  // session state
+  const { data: session } = useSession();
   // populate a list of fake users
   useEffect(() => {
     const suggestions = [...Array(20)].map((_, index) => ({
@@ -18,6 +21,10 @@ export default function Stories() {
   return (
     <div className="flex space-x-2 p-6 bg-white border-gray-200 border rounded-sm overflow-x-scroll scrollbar-thumb-black">
       {/* Stories */}
+      {/* show own individual story */}
+      {session && (
+        <Story img={session.user.image} username={session.user.username} />
+      )}
       {suggestions.map((element) => (
         <Story
           key={element.id}
